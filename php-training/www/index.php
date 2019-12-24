220e3f04ebc564565f16ddf172dcd24c7b7b2bd3
Dima Kisel <dimakisel333@gmail.com>
2019-12-24 20:21:44 +0200

    html done + variable php done

---
+++ php-training/www/index.php
@@ -0,0 +1,35 @@
+﻿<?php
+  session_start();
+  if(isset($_POST["send"])){
+    $from =htmlspecialchars($_POST["from"]);
+    $to =htmlspecialchars($_POST["to"]);
+    $subject =htmlspecialchars($_POST["subject"]);
+    $message =htmlspecialchars($_POST["message"]);
+    $_SESSION["from"] = $from;
+    $_SESSION["to"] = $to;
+    $_SESSION["subject"] = $subject;
+    $_SESSION["message"] = $message;
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Feedback</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <h1>Форма обратной связи</h1>
+    <form name="feedback" action="" method="post">
+      <label>От кого</label><br>
+      <input type="text" name="from"><br>
+      <label>Кому</label><br>
+      <input type="text" name="to" ><br>
+      <label>Тема</label><br>
+      <input type="text" name="subject" ><br>
+      <label>Сообщение</label><br>
+      <textarea name="message" cols="20" rows="10" ></textarea><br>
+      <input type="submit" name="send" value="Отправить">
+    </form>
+  </body>
+</html>
